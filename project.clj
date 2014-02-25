2867c8cdfb3796fc56a0e43b6f17ecd88f78e8f1
ewen <ewen.grosjean@gmail.com>
2014-02-26 00:45:46 +0100

    Refactoring ...

--- project.clj
+++ project.clj
@@ -13,7 +13,8 @@
                  [ring-serve "0.1.2"]
                  [ring/ring-devel "1.1.6" :scope "test"]
                  [ring/ring-core "1.1.6"]
-                 [sablono "0.2.6"]]
+                 [sablono "0.2.6"]
+                 [ewen/dragdrop "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-cljsbuild "1.0.1"]]
   :plugins [[lein-cljsbuild "1.0.1"]]
   :cljsbuild {:builds [{:id "dev"
--- src/com/ewen/cle_usb_cljs/cle_usb.cljs
+++ src/com/ewen/cle_usb_cljs/cle_usb.cljs
@@ -7,30 +7,18 @@
             [om.dom :as dom :include-macros true]
             [goog.style :as gstyle]
             [sablono.core :as html :refer-macros [html]])
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]
+                   [ewen.async-plus.macros :as async+m]))
 
 
+(enable-console-print!)
 
-
-(defn password [password-map owner]
-  (reify
-    om/IRender
-    (render [this]
-      (html [:div.password
-             [:p (:label password-map)]]))))
-
-(defn password-list [password-vect owner]
-  (reify
-    om/IRenderState
-    (render-state [this state]
-      (html [:div#list-pwd
-             (om/build-all password password-vect)]))))
 
 (defn header [app owner]
   (reify
     om/IRender
     (render [this]
-      (html [:div#action-bar
+            (html [:div#action-bar
                    [:img#logo-action-bar
                     {:src "img/logo_action_bar.png"}]
                    [:img#action-bar-divider
@@ -49,20 +37,83 @@
                      {:role "menu"
                       :aria-labelledby "dLabel"}
                      [:li
-                      [:a {:href "#"} "Home"]
-                      [:a {:href "#"} "Add new password"]]]]]))))
+                      [:a {:href "#"
+                           :on-click #(async/put!
+                                      (async/muxch* (om/get-shared owner :menu-events))
+                                      :home)}
+                       "Home"]
+                      [:a {:href "#"
+                           :on-click #(async/put!
+                                       (async/muxch* (om/get-shared owner :menu-events))
+                                       :add-new-password)}
+                       "Add new password"]]]]]))))
 
+(defn password [password-map owner]
+  (reify
+    om/IRender
+    (render [this]
+      (html [:div.password
+             [:p (:label password-map)]]))))
 
+(defn password-list [password-vect owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+      (html [:div#list-pwd
+             (om/build-all password password-vect)]))))
 
-(om/root header {}
-         {:target (-> (sel "#header")
-                      single-node)})
+(defn new-password [password-vect owner]
+  (reify
+    om/IRenderState
+    (render-state [this state]
+                  (html [:div
+                         [:div#password-label-wrapper.section
+                          [:div.section-header [:h2 "Password label"]]
+                          [:input#password-label {:placeholder "Password label" :type "text"}]]
+                         [:div#password-value-wrapper.section
+                          [:div.section-header
+                           [:h2 "Password value"]]
+                          [:input#password-value {:placeholder "Password value" :type "password"}]]
+                         [:div.action-buttons [:input#new-password-button {:disabled "disabled" :type "button" :value "Validate"}]]
+                         [:p#err-msg]]))))
+
 
 (def app-state
   (atom [{:id 1 :label "Password1"}
          {:id 2 :label "Password2"}]))
 
-(om/root password-list app-state
-         {:target (-> (sel "#app")
-                      single-node)})
+(def menu-events (async/mult (async/chan)))
+
+(defn home-view []
+  (om/root header {}
+           {:target (-> (sel "#header") single-node)
+            :shared {:menu-events menu-events}})
+
+  (om/root password-list app-state
+           {:target (-> (sel "#app") single-node)}))
+
+(defn new-password-view []
+  (om/root header {}
+           {:target (-> (sel "#header") single-node)
+            :shared {:menu-events menu-events}})
+
+  (om/root new-password
+           app-state
+           {:target (-> (sel "#app") single-node)}))
+
+(defmulti handle-menu-event identity)
+
+(defmethod handle-menu-event :home []
+  (home-view))
+
+(defmethod handle-menu-event :add-new-password []
+  (new-password-view))
+
+
+(async+m/go-loop [menu-ch menu-events]
+                 (when-let [menu-event (async/<! menu-ch)]
+                   (handle-menu-event menu-event)
+                   (recur menu-ch)))
+
+(home-view)
 
